e9d0a6eeea4cdb79c539b2967727abd1d2640214
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 19:01:36 +0500

    Ping functionality added

--- index.js
+++ index.js
@@ -101,10 +101,9 @@ var unifiedServer = function(req, res) {
 // Define the handlers
 var handlers = {};
 
-// Sample handler
-handlers.sample = function(data, callback) {
-    // Callback a HTTP status code and a payload object
-    callback(406, {'name' : 'sample handler'});
+// Ping handler
+handlers.ping = function(data, callback) {
+    callback(200);
 };
 
 // Not found handler
@@ -114,5 +113,5 @@ handlers.notFound = function(data, callback) {
 
 // Define a request router
 var router = {
-    'sample' : handlers.sample 
+    'ping' : handlers.ping 
 };
