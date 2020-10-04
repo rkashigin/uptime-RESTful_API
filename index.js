901b3cb8c0a7fc75c5914d5e2b59c7c34f3eb29e
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 17:49:01 +0500

    Response now returning JSON object

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ var server = http.createServer(function(req, res) {
             var payloadString = JSON.stringify(payload);
 
             // Return the response
+            res.setHeader('Content-Type', 'application/json');
             res.writeHead(statusCode);
             res.end(payloadString);
 
