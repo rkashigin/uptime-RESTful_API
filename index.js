1d81efdf8015b63f55cddcec0a15094f1aa3bb2d
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 16:55:59 +0500

    Server setting added

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 * Primary API file
 *
 */
+
+// Dependencies
+var http = require('http');
+
+// How server should response
+var server = http.createServer(function(req, res) {
+    res.end('Hello World\n');
+});
+
+// Start the server
+server.listen(3000, function() {
+    console.log('The server is listening on port 3000 now');
+});
