f20c1d333acefe0eec2558d59c7d07ec8bd2c710
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 18:33:26 +0500

    Added configuration of ports for https servers

--- config.js
+++ config.js
@@ -9,18 +9,20 @@ var environments = {};
 
 // Staging {default} environment
 environments.staging = {
-    'port' : 3000,
+    'httpPort' : 4000,
+    'httpsPort' : 4001,
     'envName' : 'staging'
 };
 
 // Production environment
 environments.production = {
-    'port' : 5000,
+    'httpPort' : 5000,
+    'httpsPort' : 5001,
     'envName' : 'production'
 };
 
 // Determine which environment was passed as a CLI arg
-var currentEnvironment = typeof(process.send.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+var currentEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
 
 // Check that the passed environment exists otherwise use default
 var environmentToExport = typeof(environments[currentEnvironment]) === 'object' ? environments[currentEnvironment] : environments.staging;
