3f3d51a4cefa3db467132a0e6f5bb93ddd45fb68
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-04 17:03:39 +0500

    URL parsing and logging added

--- index.js
+++ index.js
@@ -6,10 +6,24 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // How server should response
 var server = http.createServer(function(req, res) {
+    
+    // Get the URL and parse it
+    var parsedURL = url.parse(req.url, true);
+    
+    // Get the path
+    var path = parsedURL.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request received on path: ' + trimmedPath);
+    
 });
 
 // Start the server
