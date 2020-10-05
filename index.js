a659670d9015ad3764c6a88c04e5b8adbdac6bc6
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-05 14:06:50 +0500

    Tokens handler added to the router

--- index.js
+++ index.js
@@ -103,5 +103,6 @@ var unifiedServer = function(req, res) {
 // Define a request router
 var router = {
     'ping' : handlers.ping,
-    'users' : handlers.users
+    'users' : handlers.users,
+    'tokens' : handlers.tokens
 };
