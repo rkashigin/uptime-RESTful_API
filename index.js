d3391997605d55970318c32455369413228d6497
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-05 12:40:01 +0500

    CRUD functionality working fine. All tests of it deleted

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
 var fs = require('fs');
-var _data = require('./lib/data');
-
-// TESTING
-// @TODO delete this
-_data.delete('test', 'newFile', function(err) {
-    console.log('There is an error occured: ', err);
-});
 
 // Instantiate the HTTP server
 var httpServer = http.createServer(function(req, res) {
